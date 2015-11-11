def165bf1d7506bc2dd5981ed48bf2908d3690d5
Vivek Gahunia <vgahunia@gmail.com>
2015-11-11 14:10:39 -0500

    authorizarion and index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :index]
+  before_action :correct_user,   only: [:edit, :update]
   include SessionsHelper
+
   def new
   	@user = User.new
   end
@@ -33,10 +36,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.all
+  end
+
   private
 
   	def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_path
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 
 end
