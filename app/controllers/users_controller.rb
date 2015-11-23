6c754293dc09686d5171af3700d8596c1ebb3222
Vivek Gahunia <vgahunia@gmail.com>
2015-11-23 12:19:53 -0500

    Added fav_author to User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,7 @@ class UsersController < ApplicationController
   private
 
   	def user_params
-      params.require(:user).permit(:username, :first_name, :last_name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:username, :first_name, :last_name, :email, :password, :password_confirmation, :fav_author)
     end
 
     def logged_in_user
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121031554) do
+ActiveRecord::Schema.define(version: 20151123163950) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20151121031554) do
     t.string   "password_digest"
     t.string   "remember_digest"
     t.string   "username"
+    t.string   "fav_author"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
